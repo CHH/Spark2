0c414e0f7453427c3397efb827b346127c3cad8a
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-03-27 20:35:08 +0200

    Moved _autoload.php to Project root, added script for PHAR compiling

---
+++ compile.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Spark;
+
+use Phar;
+
+function print_progress()
+{
+    print ".";
+}
+
+function create_phar($distFile = "Spark.phar")
+{
+    $distPath = __DIR__ . "/dist/" . $distFile;
+    
+    if (file_exists($distPath)) {
+        unlink($distPath);
+    }
+    
+    @mkdir(__DIR__ . "/dist");
+    
+    print "Creating PHAR in $distPath...";
+    
+    $phar = new Phar($distPath, 0, "Spark");
+    $phar->setSignatureAlgorithm(Phar::SHA1);
+    $phar->startBuffering();
+    
+    print_progress();
+    
+    $files = array_merge(find_files("lib"), find_files("vendor"));
+    
+    print_progress();
+    
+    foreach ($files as $file) {
+        add_file($phar, $file);
+    }
+    
+    add_file($phar, __DIR__ . "/LICENSE.txt");
+    add_file($phar, __DIR__ . "/_autoload.php");
+    
+    $stub = <<<STUB
+<?php
+/**
+ * This file is part of the Spark Web Framework
+ *
+ * @copyright (c) 2011 Christoph Hochstrasser
+ * @license MIT License
+ */
+require_once __DIR__ . "/lib/Spark.php"; 
+STUB;
+    
+    $phar["_cli_stub.php"] = $stub;
+    $phar["_web_stub.php"] = $stub;
+    
+    $phar->setDefaultStub('_cli_stub.php', '_web_stub.php');
+    $phar->stopBuffering();
+    
+    print "Finished.\n";
+}
+
+function find_files($dir)
+{
+    $files = array();
+
+    $iterator = new \RecursiveDirectoryIterator(__DIR__ . "/$dir");
+    
+    foreach (new \RecursiveIteratorIterator($iterator) as $file) {
+        if (!$file->isFile() or substr($file->getFileName(), -4, 4) !== ".php") {
+            continue;
+        }
+        $files[] = (string) $file;
+    }
+    
+    return $files;
+}
+
+function add_file(Phar $phar, $file)
+{
+    $pharPath = str_replace(__DIR__, '', $file);
+    $content = file_get_contents($file);
+    
+    $phar->addFromString($pharPath, $content);
+    print_progress();
+}
+
+$out = isset($argv[1]) ? $argv[1] : "Spark.phar";
+create_phar($out);
